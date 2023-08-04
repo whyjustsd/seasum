9d43fe66c069808846f115b5de3ac49595931b67
whyjustsd <125178553+whyjustsd@users.noreply.github.com>
2023-08-04 10:01:37 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+ /* int a =1234;
+  cout << a <<endl;
+  char b = 'v';
+  bool c = true;
+  cout<<c<<endl;
+  double d = 12.46;
+  cout<<d<<endl;
+  int size = sizeof(a);
+  cout<<"size of a is "<<size<<endl;
+  
+  int a = 'a';
+  cout<<a<<endl;
+  char ch = 99;
+  cout<<ch<<endl;
+  float b = 2.0/5;
+  cout<<b<<endl;
+  */
+  int a =2;
+  int b = 3;
+  bool first = (a==b);
+  cout<<first<<endl;
+  int c =45;
+  cout<<!c<<endl;
+  
+}
