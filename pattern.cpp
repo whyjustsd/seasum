6cccb4e9d79015e7050e84d854fc493488bf2282
whyjustsd <125178553+whyjustsd@users.noreply.github.com>
2023-08-05 14:06:25 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,203 @@
+#include<iostream>
+using namespace std;
+int main(){
+  /*int n;
+  cin>>n;
+  int i =1 ;
+  while(i<=n){
+    int j = 1;
+    while(j<=n){
+      cout<<i;
+
+      j = j+1;
+    }
+    cout<<endl;
+    i = i+1;
+  }
+  int n;
+  cin>>n;
+  int i = 1;
+  while(i<=n){
+    int j = 1;
+    while(j<=n){
+      cout<<j;
+      j = j+1;
+    }
+    cout<<endl;
+    i = i+1;
+  }
+  int n;
+  int i=1;
+  cin>>n;
+  int count = 1;
+  while(i<=n){
+    int j=1;
+    while(j<=n){
+      cout<<count<<" ";
+      count = count+1;
+      j = j+1;
+    }cout<<endl;
+    i = i+1;
+  }
+
+  int n;
+  int i =1;
+  cin>>n;
+  while(i<=n){
+    int j =1;
+    while(j<=i){
+      cout<<"*";
+      j= j+1;
+    }cout<<endl;
+    i = i+1;
+  }
+  int n;
+  int i =1;
+  cin>>n;
+  while(i<=n){
+    int j =1;
+    while(j<=i){
+      cout<<i;
+      j= j+1;
+    }cout<<endl;
+    i = i+1;
+  }
+  int n;
+  int i =1;
+  int count =0;
+  cin>>n;
+  while(i<=n){
+    int j =1;
+    while(j<=i){
+      count = count +1;
+      cout<<count;
+      j= j+1;
+    }cout<<" "<<endl;
+    i = i+1;
+  }
+
+  int n;
+  int i = 1;
+  cin>>n;
+  while(i<=n){
+    int j = 1;
+     int value = i;
+    while(j<=i){
+      cout<<value;
+      value = value+1;
+      j= j+1;
+      
+  } cout<<endl;
+    i = i+1;
+  }
+  
+  int n;
+  int i = 1;
+  cin>>n;
+  while(i<=n){
+    int j = 1;
+    while(j<=i){
+      cout<<(i-j+1);
+      j =  j+1;
+     
+      
+  } cout<<endl;
+    i = i+1;
+   
+
+  int n ;
+  cin>>n;
+  int i  = 1;
+  while(i<= n){
+    int j =1;
+    while(j<=n){
+      char ch = 'A'+i +j-2;
+      cout<<ch;
+      j = j+1;;
+      
+    }cout<<endl;
+    i = i+1
+  
+  int n ;
+  cin>>n;
+  int i  = 1;
+  while(i<= n){
+    int j =1;
+    while(j<=i){
+      char ch = 'A'+i-1;
+      cout<<ch;
+      j = j+1;
+      
+    }cout<<endl;
+    i = i+1;
+    int n ;
+  cin>>n;
+  int i  = 1;
+  while(i<= n){
+    int j =1;
+    while(j<=i){
+      char ch = 'A'+i+j-2;
+      cout<<ch;
+      j = j+1;
+      
+    }cout<<endl;
+    i = i+1;
+    int n ;
+  cin>>n;
+  int i  = 1;
+  while(i<= n){
+    int j =1;
+    while(j<=i){
+      char ch = 'D'-i+j;
+      //'A'+
+      cout<<ch;
+      j = j+1;
+      
+    }cout<<endl;
+    i = i+1;
+
+  int n ;
+  cin>>n;
+  int row = 1;
+  while(row<=n){
+    int space = n-row;
+    while(space){
+      cout<<" ";
+      space = space -1;
+    }
+    int col = 1;
+    while(col <= row){
+      cout<<"*";
+      col = col+1;
+    }
+    cout<<endl;
+    row = row+1;
+    
+  int n ;
+  cin>>n;
+  int row = 1;
+  while(row<=n){  
+    int space= n - row ;
+    while(space){
+      cout<<" ";
+      space = space -1;
+    }
+    int col = 1;
+    while(col <= row){
+      cout<<col;
+      col = col+1;
+    }
+    int start = row -1;
+    while(start){
+      cout<<start;
+      start = start -1;
+    }
+    
+    cout<<endl;
+    row = row+1;
+
+
+  }
+      
+  }
+*/
