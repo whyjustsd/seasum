c0b2571ff378261c8e1005c25e535301da4f6e3c
whyjustsd <125178553+whyjustsd@users.noreply.github.com>
2023-08-06 22:35:05 +0530

    Add files via upload

---
+++ operator and for.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   /* int a = 4;
+    int b = 6;
+    cout<<"a&b "<< (a&b)<<endl;
+    cout<<"a|b "<< (a|b)<<endl;
+    cout<<"~a" << (~a)<<endl;
+    cout<<"a^b "<< (a^b)<<endl;
+    cout<<(17>>1)<<endl;
+    cout<<(17>>2)<<endl;
+    cout<<(19<<1)<<endl;
+    cout<<(19<<2)<<endl;
+    int i = 7;
+
+    cout<<(++i)<<endl;
+    cout<<(i--)<<endl;
+    cout<<(i++)<<endl;
+    cout<<(i--)<<endl;
+    stage1
+    100
+    1,2
+    
+
+    int a=  1 ;
+    int b =  a++;
+    int c = ++a;
+    cout<<b;
+    cout<<c;
+    
+
+   int n;
+   cout<<"enter the value of n"<<endl;
+   cin>>n;
+   cout<<"print the sum"<<endl;
+   int sum = 0;
+   
+   for(int i = 1; i<=n;i++){
+    sum = sum+i;
+   }
+   cout<<sum<<endl;
+
+   //finnobaci
+   int n = 15;
+
+   int a = 0;
+   int b = 1;
+   cout<<a <<b <<" ";
+   for(int i = 0;i<=n;i++){
+    int nextsum = a+b;
+    cout<<nextsum<< " ";
+    a =b;
+    b = nextsum;
+   }
+
+
+int n;
+cout<<"enter the number"<<endl;
+cin>>n;
+bool isPrime=1;
+for(int i = 2; i <n; i++){
+    if(n%i ==0){
+        if(n%i ==0){
+            isPrime=0;
+            break;
+        }
+        
+    }
+    if(isPrime==0){
+        cout<<"not a prime nummber"<<endl;
+    }
+    else
+    {
+        cout<<"is a prime number"<<endl;
+    }
+    
+
+}
+
+for(int i = 0; i<=5;i++){
+    cout<<"hi";
+    cout<<"hey";
+    continue;
+    cout<<"<bye";
+}
+
+0
+1
+2
+3
+4
+5
+0 0...
+1234
+
+
+
+
+for(int i = 0; i<5; i++){
+    for(int j =i;j<=5;j++){
+        cout<<i<<" "<<j<<endl;
+    }
+}
+*/
+
+
+
+}
+
