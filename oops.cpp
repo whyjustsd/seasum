18e7d4f8aa9a054e781533a11427add22dd54538
whyjustsd <125178553+whyjustsd@users.noreply.github.com>
2023-09-18 13:38:20 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+    void eat(){
+        cout<<"eating"<<endl;
+    }
+};
+class dog: public Animal{
+    public: void bark(){
+        cout<<"barking"<<endl;
+    }
+};
+
+class cat: public Animal{
+    public: void meow(){
+        cout<<"meowing"<<endl;
+    }
+};
+int main(){
+    dog sd;
+    sd.eat();
+    sd.bark();*/
+
